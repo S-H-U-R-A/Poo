a9514abab8803f9f6d478062e534ea4da3222ea1
srodriguez9017 <sandres9017@gmail.com>
2020-05-07 14:56:44 -0500

    Avances Correcion

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,6 @@
 import model.Camera
+import model.Movie
+import model.Shoe
 
 fun main(args: Array<String>){
 
@@ -7,5 +9,20 @@ fun main(args: Array<String>){
     println( camera.getCameraStatus() )
     camera.setResolution(1080)
     println( "La resolucion de la camara es: ${ camera.getResolution() }")
+
+    var shoe:Shoe = Shoe("shoe","Blue Shoes",12345, "Praga")
+    shoe.size = 39
+    println(shoe.size)
+
+    shoe.model = "Tennis"
+    println(shoe.model)
+
+    println("Shoe: \n" + shoe)
+
+    val movie:Movie = Movie("Cocodrilo", "Sony", 120)
+    println("\nPelicula")
+    println(movie.title)
+    println(movie.creator)
+    println(movie.duration)
 
 }
